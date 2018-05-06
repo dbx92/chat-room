6ba4deb6c8697a98f8d2bbdca12d524ea8b68abe
dbx92 <davidpiobergamo@comcast.net>
2018-05-05 20:24:57 -0400

    create-chat-rooms

--- src/components/RoomList.js
+++ src/components/RoomList.js
@@ -5,12 +5,21 @@ class RoomList extends Component{
   constructor(props){
     super(props);
     this.state = {
-      rooms: [ ]
+      rooms: [ ],
+      newRoomName:""
     };
       this.roomsRef = this.props.firebase.database().ref('rooms');
-  }
-deleteChatRoom(){
+      this.textEnter = this.textEnter.bind(this);
 
+  }
+createChatRoom(){
+  console.log("chatroom created");
+  this.roomsRef.push({name:this.state.newRoomName});
+  this.setState({newRoomName:""})
+}
+textEnter(e){
+  console.log(e.target.value);
+  this.setState({newRoomName:e.target.value})
 }
 
 componentDidMount(){
@@ -21,17 +30,23 @@ componentDidMount(){
     this.setState({rooms:this.state.rooms.concat(room)})
   });
 }
+
+printRoomName(name){
+  console.log(name);
+}
   render(){
     return(
       <div className="rooms">
       <h1>"Rooms"</h1>
       {this.state.rooms.map((room, i) => (
-        <div key={i}>
+        <div key={i} onClick={() => this.printRoomName(room.name)}>
           {room.name}
         </div>
       ))}
+        <input type="text" value={this.state.newRoomName} onChange={this.textEnter}/>
+        <button onClick={() => this.createChatRoom()}>X </button>
           </div>
-          );
+            );
 }
 }
 
